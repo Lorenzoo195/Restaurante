79bd5e706b339a5b8e6e6ad02eb00a98026c6a9c
Lorenzoo195 <lorenzoactur@gmail.com>
2026-09-24 19:13:56 +0200

    Añadida clase Bebida

---
+++ Models/Bebida.cs
@@ -0,0 +1,16 @@
+using Models;
+
+public class Bebida : Producto
+{
+    public bool EsAlcoholica {get; set;}
+
+public Bebida (string nombre, double precio, bool esAlcoholica): base (nombre, precio)
+    {
+        EsAlcoholica = esAlcoholica;
+    }
+    public override string ObtenerDescripcion()
+    {
+        string tipo = EsAlcoholica ? "Con alcohol" : "Sin alcohol";
+        return $"{Nombre} - {Precio} EUR ({tipo})";
+    }
+}
