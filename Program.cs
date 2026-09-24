10c2703f4434fe6a355db8fa93e710786a001fd9
Lorenzoo195 <lorenzoactur@gmail.com>
2026-09-24 18:24:15 +0200

    Guardado entrantes en Program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+using Restaurante.Models;
+
+Entrante bravas = new Entrante("Patatas bravas", 6.50, 2, false);
+Entrante nachos = new Entrante("Nachos con queso", 8.00, 3, false);
+Entrante ensaladilla = new Entrante("Ensaladilla rusa", 5.00, 2, true);
+
+Console.WriteLine(bravas.ObtenerDescripcion());
+Console.WriteLine(nachos.ObtenerDescripcion());
+Console.WriteLine(ensaladilla.ObtenerDescripcion());
